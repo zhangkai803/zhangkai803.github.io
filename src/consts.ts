5b595d44183aad125e67d042043c1c192df36eb1
zk <kai.zhang@lizhiweike.com>
2023-05-22 10:16:35 +0800

    Revert "dev: test title"
    This reverts commit ba4951b689a5f78a48917ae3808258366df9f382.

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-// export const SITE_TITLE = "Kai's personal website build with astro.";
-export const SITE_TITLE = "荔枝微课";
+export const SITE_TITLE = "Kai's personal website build with astro.";
 export const SITE_DESCRIPTION = "Welcome to my website!";
